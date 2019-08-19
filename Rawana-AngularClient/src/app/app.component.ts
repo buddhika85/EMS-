5ee5836da6e17766f11cb16c0cdb5f82d7e202ff
Buddhika85 <buddhika2006system@gmail.com>
2019-08-20 04:31:16 +0530

    Angular client 1 with Routing

---
+++ Rawana-AngularClient/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ems-root',
+  templateUrl: './app.component.html',
+  styles: []
+})
+export class AppComponent
+{
+  pageTitle : string = 'EMS - Angular Client';
+}
---
+++ Rawana-AngularClient/src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from "@angular/common/http";
+import { RouterModule } from "@angular/router";
+
+
+import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { EmployeeDetailComponent } from './employee-detail/employee-detail.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    WelcomeComponent,
+    EmployeeListComponent,
+    EmployeeDetailComponent,
+    
+  
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      {path : 'employees', component: EmployeeListComponent},
+      {path : 'employee/:id', component: EmployeeDetailComponent},
+      {path : 'welcome', component: WelcomeComponent},
+      {path : '', redirectTo : 'welcome', pathMatch : 'full'},
+      {path : '**', redirectTo : 'welcome', pathMatch : 'full'}
+    ])
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
